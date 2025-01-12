5555c3b196e9ebd87bb42d55c15e770bac0eae07
BHAVESH KRISHAN GARG <72064250+bhaveshgarg2000@users.noreply.github.com>
2025-01-12 21:36:32 +0530

    Image in Home Page (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import {} from 'react';
 import AppBar from './Components/AppBar'
 
 function App() {
+  const style = {
+    height: '60%',
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  };
 
   return (
     <>
       <AppBar/>
-      
+      <img src="https://pbs.twimg.com/media/E4Ph7CZVUA0SbIC.jpg:large" alt="Placeholder Image"
+
+    style={style}    
+      />
+
+
     </>
   )
 }
